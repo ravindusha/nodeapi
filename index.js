c4004d1bd2320bb2b630d32728a76921b5051daa
ravindusha <ravzshanaka@gmail.com>
2018-02-25 22:22:48 +0530

    Initial test

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+//express app
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req,res)=>{
+  res.json({
+    "message": "Welcome to simple Node.js API"
+  });
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000...");
+})
